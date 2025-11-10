8b0d132eb786d1e72e521470fb9f04b5bdc8769e
Malloy <li3210361050@gmail.com>
2025-11-10 17:53:38 +0800

    chore: ssr改为了false 暂时

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from "@tailwindcss/vite"
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  ssr: false,
   devtools: { enabled: true },
   css: ['./app/assets/css/main.css'],
   vite: {
